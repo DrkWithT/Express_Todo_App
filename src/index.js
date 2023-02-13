5e56fd3ee9730935bb2c73668aadaa73bf39cffd
drkwitht <derektan@gmail.com>
2023-02-12 21:28:14 -0800

    Updated code with insert or delete ops.

--- src/index.js
+++ src/index.js
@@ -22,7 +22,12 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
     let appAction = parseInt(req.body.action) || 0;
     let todoTitle = req.body.title || 'foo';
+    let todoID = -1;
     let todoDescription = req.body.description;
+
+    if (!isNaN(req.body.title)) {
+        todoID = parseInt(req.body.title); // if a valid ID is passed instead of a title, use it for deletions only!
+    }
 
     let realTitle = todoTitle.trim().split(' ').join('_'); // format title string with snake case
 
@@ -40,8 +45,32 @@ app.post('/', (req, res) => {
                 }
             });
             break;
-        case 2: // TODO: add an insertion action of a todo task.
-        case 3: // TODO: add a deletion action of a todo task.
+        case 2: // add a todo task...
+            mydatabase.insertTodoTask({title: realTitle, description: todoDescription}, (err, data) => {
+                if (err) {
+                    console.error(`SQL Error: ${err.message}`);
+
+                    res.render('landing', {page_title: 'Todos', result_title: 'Error', result_text: 'Failed to add the task.'});
+                } else if (data.ok) {
+                    res.render('landing', {page_title: 'Todos', result_title: data.title.replace('_', ' '), result_text: 'Added task.'});
+                } else {
+                    res.render('landing', {page_title: 'Todos', result_title: 'Error', result_text: 'Failed to add the task.'});
+                }
+            });
+            break;
+        case 3: // delete a todo task...
+            mydatabase.deleteTodoTask({title: realTitle, id: todoID}, (err, data) => {
+                if (err) {
+                    console.error(`SQL Error: ${err.message}`);
+
+                    res.render('landing', {page_title: 'Todos', result_title: 'Error', result_text: 'Failed to delete new task.'});
+                } else if (data.ok) {
+                    res.render('landing', {page_title: 'Todos', result_title: data.title.replace('_', ' '), result_text: 'Deleted task.'});
+                } else {
+                    res.render('landing', {page_title: 'Todos', result_title: 'Error', result_text: 'Failed to delete new task.'});
+                }
+            });
+            break;
         default:
             res.render('landing', {page_title: 'Todos', result_title:'Error', result_text: 'Unsupported action.'});
             break;
